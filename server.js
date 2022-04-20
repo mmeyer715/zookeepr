6fa7b454378043753e50b169641881ee431bcb91
Maria Meyer <mbean1216@icloud.com>
2022-04-19 21:40:00 -0500

    port updates

--- server.js
+++ server.js
@@ -32,6 +32,11 @@ function filterByQuery(query, animalsArray) {
     }
     return filteredResults;
 }
+
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+}
 // creating route that front-end can request data from
 app.get('/api/animals', (req, res) => {
     let results = animals;
@@ -41,8 +46,17 @@ app.get('/api/animals', (req, res) => {
     res.json(results);
 });
 
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
+});
+
 // method allowing server to listen to requests
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
-});
+  });
 
