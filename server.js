f9e3195678ba80cb66022bdd3b9118cae401c863
Maria Meyer <mbean1216@icloud.com>
2022-04-19 21:03:56 -0500

    Add Heroku

---
+++ server.js
@@ -0,0 +1,47 @@
+// required packages
+const express = require('express');
+const { animals } = require('./data/animals');
+
+const app = express();
+
+// filtering results
+function filterByQuery(query, animalsArray) {
+    let personalityTraitsArray = [];
+    let filteredResults = animalsArray;
+    if (query.personalityTraits) {
+        if (typeof query.personalityTraits === 'string') {
+            personalityTraitsArray = [query.personalityTraits];
+        } else {
+            personalityTraitsArray = query.personalityTraits;
+        }
+        personalityTraitsArray.forEach(trait => {
+            filteredResults = filteredResults.filter(
+                animal => animal.personalityTraits.indexOf(trait) !== -1
+            );
+        });
+    }
+    if (query.diet) {
+      filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
+    }
+    if (query.species) {
+      filteredResults = filteredResults.filter(animal => animal.species === query.species);
+    }
+    if (query.name) {
+      filteredResults = filteredResults.filter(animal => animal.name === query.name);
+    }
+    return filteredResults;
+}
+// creating route that front-end can request data from
+app.get('/api/animals', (req, res) => {
+    let results = animals;
+    if (req.query) {
+        results = filterByQuery(req.query, results);
+    }
+    res.json(results);
+});
+
+// method allowing server to listen to requests
+app.listen(3001, () => {
+    console.log('API server now on port 3001!');
+});
+
